dd873ebfbb0a08e2fe9fbe9348934a4d35205afa
ZARIF ubuntu <uzbzarif@gmail.com>
2021-09-26 13:10:03 +0900

    comment

--- create_pascal_tfrecord.py
+++ create_pascal_tfrecord.py
@@ -31,36 +31,36 @@ from lxml import etree
 import PIL.Image
 import tensorflow as tf
 
-from dataset import tfrecord_util
+import tfrecord_util
 
 FLAGS = flags.FLAGS
 
 SETS = ['train', 'val', 'trainval', 'test']
 YEARS = ['VOC2007', 'VOC2012', 'merged']
 
-pascal_label_map_dict = {
-    'background': 0,
-    'aeroplane': 1,
-    'bicycle': 2,
-    'bird': 3,
-    'boat': 4,
-    'bottle': 5,
-    'bus': 6,
-    'car': 7,
-    'cat': 8,
-    'chair': 9,
-    'cow': 10,
-    'diningtable': 11,
-    'dog': 12,
-    'horse': 13,
-    'motorbike': 14,
-    'person': 15,
-    'pottedplant': 16,
-    'sheep': 17,
-    'sofa': 18,
-    'train': 19,
-    'tvmonitor': 20,
-}
+# pascal_label_map_dict = {
+#     'background': 0,
+#     'aeroplane': 1,
+#     'bicycle': 2,
+#     'bird': 3,
+#     'boat': 4,
+#     'bottle': 5,
+#     'bus': 6,
+#     'car': 7,
+#     'cat': 8,
+#     'chair': 9,
+#     'cow': 10,
+#     'diningtable': 11,
+#     'dog': 12,
+#     'horse': 13,
+#     'motorbike': 14,
+#     'person': 15,
+#     'pottedplant': 16,
+#     'sheep': 17,
+#     'sofa': 18,
+#     'train': 19,
+#     'tvmonitor': 20,
+# }
 
 
 def define_flags():
